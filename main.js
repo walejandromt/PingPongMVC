a3af41dcc1ddac1571de3a794ec5b7348c7c1402
Williams Alejandro <walejandromt@gmail.com>
2016-06-24 13:56:15 -0500

    Se agregaron nuevos objetos y se corrigen errores detectados

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 	self.Board.prototype = {
 		get elements(){
 			var elements = this.bars;
-			elements.push(ball);
+			elements.push(this.ball);
 			return elements;
 		}
 	}
@@ -26,9 +26,10 @@
 		this.board = board;
 		this.board.bars.push(this);
 		this.kind = "rectangle";
+
 	}
 
-	self.Board.prototype ={
+	self.Bar.prototype ={
 		down: function(){
 
 		},
@@ -36,22 +37,46 @@
 
 		}
 	}
-});
+})();
+
 (function(){
 	self.BoardView = function(canvas, board){
 
 		this.canvas = canvas;
 		this.canvas.width = board.width;
 		this.canvas.height = board.height;
+		this.board = board;
 		this.ctx = canvas.getContext("2d");
 		}
-		
+		self.BoardView.prototype = {
+			draw: function(){
+				for (var i = this.board.elements.length - 1; i >= 0; i++){
+					var el = this.board.elements[i];
+
+					draw(this.ctx, el);
+				};
+			}
+		}
+		function draw(ctx, element){
+			if(element !== null && element.hasOwnProperty("kind")){
+				switch(element.kind){
+				case "rectangle":
+				console.log("hola");
+				ctx.fillRect(element.x,element.y,element.width,element.height);
+				break;
+			}
+			}
+			
+		}
 })();
 
-self.addEventListener("Load", main);
+window.addEventListener("load", main);
 
 function main(){
 	var board = new Board(800, 400);
+	var bar = new Bar(20,100,40,100,board);
 	var canvas = document.getElementById('canvas');
 	var board_view = new BoardView(canvas, board);
+
+	board_view.draw();
 }
